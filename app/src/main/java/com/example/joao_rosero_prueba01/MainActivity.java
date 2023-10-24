398daa570741840b66cc8ed6b78fbf1924506341
FISEI-LB1-PC17 <joaorosero1a@hotmail.com>
2023-10-24 17:31:21 -0500

    Joao Rosero 17:30

--- app/src/main/java/com/example/joao_rosero_prueba01/MainActivity.java
+++ app/src/main/java/com/example/joao_rosero_prueba01/MainActivity.java
@@ -4,12 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextName;
+    private EditText editTextLastName;
+    private Button buttonNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        editTextName = findViewById(R.id.editTextName);
+        editTextLastName = findViewById(R.id.editTextLastName);
+        buttonNext = findViewById(R.id.buttonNext);
+
+        buttonNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //
+                editTextName.setEnabled(true);
+                editTextLastName.setEnabled(true);
+            }
+        });
     }
 }
