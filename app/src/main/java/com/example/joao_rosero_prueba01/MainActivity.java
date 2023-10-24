13883a8b27e66135ea2b5f17ce46f7122f54b544
FISEI-LB1-PC17 <joaorosero1a@hotmail.com>
2023-10-24 18:45:28 -0500

    Joao Rosero 18:45

--- app/src/main/java/com/example/joao_rosero_prueba01/MainActivity.java
+++ app/src/main/java/com/example/joao_rosero_prueba01/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.joao_rosero_prueba01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import android.view.View;
@@ -11,7 +10,7 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText editTextName;
+    private EditText editTextTextName;
     private EditText editTextLastName;
     private Button buttonNext;
 
@@ -20,30 +19,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Referencias
 
-
-        buttonNext.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //
-                editTextName.setEnabled(true);
-                editTextLastName.setEnabled(true);
-            }
-        });
-
-    }
-    public  void onClickView(View view){
-        String TextName = editTextName.getText().toString();
-        String TextLastName = editTextLastName.getText().toString();
-
-        editTextName.setEnabled(true);
-        editTextLastName.setEnabled(true);
-
-    }
-
-    public void onClickSecondActivity(View view){
-        Intent intent = new Intent(this, activity_third.class);
-        startActivity(intent);
     }
 }
