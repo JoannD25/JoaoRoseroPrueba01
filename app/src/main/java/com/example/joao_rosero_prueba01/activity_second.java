1e74fec6917bfecf3ca8a5c7db59b3b87e4c1123
FISEI-LB1-PC17 <joaorosero1a@hotmail.com>
2023-10-24 18:15:19 -0500

    Joao Rosero 18:15

--- app/src/main/java/com/example/joao_rosero_prueba01/activity_second.java
+++ app/src/main/java/com/example/joao_rosero_prueba01/activity_second.java
@@ -2,7 +2,9 @@ package com.example.joao_rosero_prueba01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class activity_second extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class activity_second extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+    }
+
+    public void onClickSecondActivity(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
