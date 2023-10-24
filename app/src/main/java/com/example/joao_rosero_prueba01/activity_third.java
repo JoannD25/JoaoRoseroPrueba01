e6157805a8cf689480c0d34cc42ddcf7a4789ce9
FISEI-LB1-PC17 <joaorosero1a@hotmail.com>
2023-10-24 17:16:57 -0500

    Joao Rosero 17:15

---
+++ app/src/main/java/com/example/joao_rosero_prueba01/activity_third.java
@@ -0,0 +1,14 @@
+package com.example.joao_rosero_prueba01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_third extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+    }
+}
